ca14630f1c6ce910de3a59d455c2acd676972ccd
Jun <email2mrjunlee@gmail.com>
2021-02-01 19:22:07 +1300

    css projects filtering

--- main.js
+++ main.js
@@ -61,6 +61,30 @@ arrowBtn.addEventListener('click', () => {
   scrollIntoView('#home')
 })
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (event) => {
+  const target = event.target;
+  const filter = target.dataset.filter || event.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (project.dataset.type == filter || filter == "*") {
+        project.classList.remove('project--invisible')
+      } else {
+        project.classList.add('project--invisible')
+      }
+    })
+    projectContainer.classList.remove('anim-out');
+
+  }, 300);
+})
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
